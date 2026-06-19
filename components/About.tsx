fb01a0aee184b297617ebff7ecc0ec9bc1fdf91b
Habib Birhanu <HabibB@ZAMZAMBANK.LOCAL>
2026-06-19 15:00:05 +0300

    remove image

--- components/About.tsx
+++ components/About.tsx
@@ -2,22 +2,11 @@ export default function About() {
   return (
     <section id="about" className="bg-[#0f0f0f] text-white py-28 px-6">
 
-      <div className="max-w-6xl mx-auto grid md:grid-cols-2 gap-16 items-center">
+      <div className="max-w-6xl mx-auto grid md:grid-cols-1 gap-16 items-center">
 
         <div className="relative flex justify-center items-center">
 
-  {/* Glow background */}
-  <div className="absolute w-72 h-72 bg-blue-500/20 rounded-full blur-3xl"></div>
-
-  {/* Circle frame */}
-  <div className="relative w-75 h-75 rounded-full border-4 border-blue-500 overflow-hidden shadow-[0_0_50px_rgba(59,130,246,0.7)]">
-
-    <img
-      src="/profile.jpg"
-      alt="Habib Profile"
-className="w-full h-full object-cover scale-100 object-top"    />
-
-  </div>
+ 
 
 </div>
 
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-white text-center py-6">
-      <p>© 2026 Habib Birhanu
-Built with Next.js & Tailwind CSS.</p>
+      <p>© 2026 Habib Birhanu. All rights reserved.</p>
     </footer>
   )
 }
